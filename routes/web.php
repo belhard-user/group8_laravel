aef6a9363b62c2ccde7bb15a943fcdf40ee95e59
unknown <ldarik2l@gmail.com>
2016-11-05 20:39:00 +0200

    clear project

--- routes/web.php
+++ routes/web.php
@@ -14,47 +14,4 @@ Route::group(['prefix' => 'auto'], function($router){
 Route::get('/', ['uses'=>'HomeController@home', 'as' => 'home']);
 Route::get('about', ['uses' => 'HomeController@about', 'as' => 'home.about']);
 
-Route::get('insert', 'DBController@insert');
-Route::get('delete', 'DBController@delete');
-Route::get('update', 'DBController@update');
-Route::get('select', 'DBController@select');
-Route::get('forms', 'DBController@forms');
-Route::post('forms', 'DBController@postForm');
-Route::get('model', 'TestController@model');
-Route::post('model', 'TestController@postModel');
-Route::get('testdrive', 'TestController@testdrive');
-Route::get('relations', 'TestController@relations');
-Route::get('addRelations', 'TestController@addRelations');
-
-
-
-
-
-/*Route::get('/', function(){
-    return 'Main';
-});
-
-Route::get('about', function(){
-    return 'about';
-});
-
-Route::match(['post', 'get'], 'test', function(){
-    return 'oOOooOOo';
-});
-
-
-Route::group(['prefix' => 'admin'], function($router){
-
-    $router->get('/', function(){
-        return 'Admin Main';
-    });
-
-    $router->get('/about', function(){
-        return 'Admin about';
-    });
-
-});*/
-
 Auth::routes();
-
-Route::get('/home', 'HomeController@index');
